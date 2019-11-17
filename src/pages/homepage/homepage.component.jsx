33d25881604a824d8e5923db1393f39ae9b9bca5
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-11-17 13:22:49 -0800

    initial commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-list'>
+      <div className='list-category'>
+        <div className='content'>
+          <h1 className='title'>TITLE</h1>
+        </div>
+      </div>
+    </div>
+    <div className='directory-list'>
+      <div className='list-category'>
+        <div className='content'>
+          <h1 className='title'>TITLE</h1>
+        </div>
+      </div>
+    </div>
+    <div className='directory-list'>
+      <div className='list-category'>
+        <div className='content'>
+          <h1 className='title'>TITLE</h1>
+        </div>
+      </div>
+    </div>
+    <div className='directory-list'>
+      <div className='list-category'>
+        <div className='content'>
+          <h1 className='title'>TITLE</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
---
+++ src/redux/data/data.js
@@ -0,0 +1,189 @@
+const initialListData = {
+  fruits: {
+    id: 1,
+    title: 'FRUITS',
+    routeName: 'fruits',
+    items: [
+      {
+        id: 1,
+        name: 'Apples',
+        checked: false
+      },
+      {
+        id: 2,
+        name: 'Bananas',
+        checked: false
+      },
+      {
+        id: 3,
+        name: 'Oranges',
+        checked: false
+      },
+      {
+        id: 4,
+        name: 'Strawberries',
+        checked: false
+      },
+      {
+        id: 5,
+        name: 'Grapes',
+        checked: false
+      },
+    ]
+  },
+  vegetables: {
+    id: 1,
+    title: 'VEGETABLES',
+    routeName: 'vegetables',
+    items: [
+      {
+        id: 1,
+        name: 'Broccoli',
+        checked: false
+      },
+      {
+        id: 2,
+        name: 'Carrots',
+        checked: false
+      },
+      {
+        id: 3,
+        name: 'Cucumbers',
+        checked: false
+      },
+      {
+        id: 4,
+        name: 'Garlic',
+        checked: false
+      },
+      {
+        id: 5,
+        name: 'Lettuce',
+        checked: false
+      },
+      {
+        id: 6,
+        name: 'Mushrooms',
+        checked: false
+      },
+      {
+        id: 7,
+        name: 'Onions',
+        checked: false
+      },
+      {
+        id: 8,
+        name: 'Peppers',
+        checked: false
+      },
+      {
+        id: 9,
+        name: 'Potatoes',
+        checked: false
+      },
+      {
+        id: 10,
+        name: 'Tomatoes',
+        checked: false
+      },
+    ]
+  },
+  dairy: {
+    id: 1,
+    title: 'DAIRY',
+    routeName: 'DAIRY',
+    items: [
+      {
+        id: 1,
+        name: 'Milk',
+        checked: false
+      },
+      {
+        id: 2,
+        name: 'Butter',
+        checked: false
+      },
+      {
+        id: 3,
+        name: 'Eggs',
+        checked: false
+      },
+      {
+        id: 4,
+        name: 'Cheese',
+        checked: false
+      },
+      {
+        id: 5,
+        name: 'Yogurt',
+        checked: false
+      },
+    ]
+  },
+  Meat: {
+    id: 1,
+    title: 'MEAT',
+    routeName: 'meat',
+    items: [
+      {
+        id: 1,
+        name: 'Beef',
+        checked: false
+      },
+      {
+        id: 2,
+        name: 'Chicken',
+        checked: false
+      },
+      {
+        id: 3,
+        name: 'Pork',
+        checked: false
+      },
+      {
+        id: 4,
+        name: 'Fish',
+        checked: false
+      },
+      {
+        id: 5,
+        name: 'Bacon',
+        checked: false
+      },
+    ]
+  },
+  breads: {
+    id: 1,
+    title: 'breads',
+    routeName: 'breads',
+    items: [
+      {
+        id: 1,
+        name: 'Sandwich',
+        checked: false
+      },
+      {
+        id: 2,
+        name: 'Bagles',
+        checked: false
+      },
+      {
+        id: 3,
+        name: 'Fresh Loaf',
+        checked: false
+      },
+      {
+        id: 4,
+        name: 'Dinner Rolls',
+        checked: false
+      },
+      {
+        id: 5,
+        name: 'Tortillas',
+        checked: false
+      },
+    ]
+  },
+}
+
+export default initialListData;
